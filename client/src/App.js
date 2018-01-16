80aa6124c1c13010654a81b6c36703ec9674142a
Josue Perez <che.perez3@gmail.com>
2018-01-16 16:42:24 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import { subscribeToTimer } from './api';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    subscribeToTimer((timestamp) => {
+      this.setState({
+        timestamp
+      });
+    });
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  };
+
+  render() {
+    return (
+      <div className="App">
+        This is the value of the timer timestamp: {this.state.timestamp}      
+      </div>
+    );
+  }
+}
+
+export default App;
