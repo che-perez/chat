825b97343a152d701cdebdb674131861cdfb4ac0
Danny Eng <dannyeng94@gmail.com>
2018-01-16 18:27:42 -0500

    pre-rebase

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react'
 import './App.css'
 
 import { subscribeToTimer } from './api'
-
+import ChannelList from './components/ChannelList'
 import Chat from './components/Chat'
 import UserList from './components/UserList'
-import ChannelList from './components/ChannelList';
 
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       timestamp: 'no timestamp yet'
     }
-
     subscribeToTimer((timestamp) => {
       this.setState({
         timestamp
--- client/src/components/Channel.jsx
+++ client/src/components/Channel.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 
 function Channel (props){
-    return (
-        <div className="Channel">
-            <ul>
-                <li>Channel-1</li>
-                <li>Channel-2</li>
-                <li>Channel-3</li>
-            </ul>
-        </div>
-    )
-
+  return (
+      <div className="Channel">
+          <ul>
+              <li>Channel-1</li>
+              <li>Channel-2</li>
+              <li>Channel-3</li>
+          </ul>
+      </div>
+  )
 }
-
-export default Channel;
+export default Channel
