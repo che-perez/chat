346452114d85e70ba783f579bafae703a4af8e85
Danny Eng <dannyeng94@gmail.com>
2018-01-16 16:20:17 -0500

    removed more semicolons

--- client/src/api.js
+++ client/src/api.js
@@ -1,12 +1,12 @@
-import openSocket from 'socket.io-client';
+import openSocket from 'socket.io-client'
 
-const socket = openSocket('http://localhost:8000');
+const socket = openSocket('http://localhost:8000')
 
 function subscribeToTimer(cb) {
-  socket.on('timer', timestamp => cb(timestamp));
-  socket.emit('subscribeToTimer', 1);
+  socket.on('timer', timestamp => cb(timestamp))
+  socket.emit('subscribeToTimer', 1)
 }
 
 export {
   subscribeToTimer,
-};
+}
