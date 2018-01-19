5b38eeec0561a7cd1834a6c02543724e3972a53a
Danny Eng <dannyeng94@gmail.com>
2018-01-19 12:31:22 -0500

    random colors assigned to each unique user

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,18 +1,78 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Message from './Message'
 
-function Chat (props){
-  return(
-    <div className="chat">
-      <div className="logo-container">
-        <p id="chat-logo">Chattish</p>
+class Chat extends Component {
+
+  constructor(props){
+    super()
+    this.state = {
+      messages: [{
+        id: 1,
+        content: "hi",
+        user: "Mofongo"
+      },
+      {
+        id: 2,
+        content: "hello",
+        user: "peter"
+      },
+      {
+        id: 3,
+        content: "hi",
+        user: "thompsom"
+      },
+      {
+        id: 4,
+        content: "no get out",
+        user: "Mofongo"
+      },
+      {
+        id: 5,
+        content: "why D:",
+        user: "peter"
+      }],
+      users: ["Mofongo", "peter", "thompson"],
+      colors: {}
+    }
+    this.assignRandomColor = this.assignRandomColor.bind(this)
+  }
+
+  componentDidMount(){
+    this.assignRandomColor()
+  }
+
+  assignRandomColor(){
+    let colors = {}
+    for (let i = 0; i < this.state.users.length; i++){
+      colors[this.state.users[i]] = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16)
+    }
+    this.setState({
+      colors: colors
+    })
+  }
+
+  render(){
+    return(
+      <div className="chat">
+        <div className="logo-container">
+          <p id="chat-logo">Chattish</p>
+        </div>
+        {this.state.messages.map(message => {
+          return(
+            <Message
+              key={message.id}
+              owner={message.user}
+              content={message.content}
+              color={this.state.colors[message.user]}
+            />
+          )
+        })}
       </div>
-      <Message owner="Mofongo" content="Hi." />
-      <Message owner="peter" content="sup!" />
-      <Message owner="Mofongo" content="not much" />
-    </div>
-  )
+    )
+  }
+
+
 }
 
 export default Chat
--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -4,31 +4,30 @@ import React, { Component } from 'react'
 
 
 class Message extends Component {
- constructor(props){
+  constructor(props){
    super(props)
    this.state = {
      checking: false
    }
    this.getRandomColor = this.getRandomColor.bind(this);
- }
- getRandomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
- }
-render(){
+  }
+  getRandomColor() {
+    return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  }
+  render(){
+    return (
+      <div className="message" style={{backgroundColor: `${this.props.color}`}}>
 
-  return (
-    <div className="message" style={{backgroundColor: `${this.getRandomColor()}`}}>
-    
-      <div className="message-owner">
-        <p>{this.props.owner}: </p>
-        
+        <div className="message-owner">
+          <p>{this.props.owner}: </p>
+
+        </div>
+        <div className="message-content">
+          <p>{this.props.content}</p>
+        </div>
       </div>
-      <div className="message-content">
-        <p>{this.props.content}</p>
-      </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 
