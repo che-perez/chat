7a7af492774757d3050ec21ac35c465e7c80aa98
Danny Eng <dannyeng94@gmail.com>
2018-01-19 13:50:28 -0500

    fixing the chat

--- client/src/components/MessageBox.jsx
+++ client/src/components/MessageBox.jsx
@@ -18,14 +18,12 @@ class MessageBox extends Component {
     console.log('from box', this.props)
     return (
       <div className="message-box">
-      <div>
         <form onSubmit={this.handleSubmit}>
           <input type="text" value={this.state.message}
             onChange={(evt) => this.setState({ message: evt.target.value })}
             placeholder="Writte" required />
           <button type="submit">Send</button>
         </form>
-      </div>
       </div>
     )
   }
