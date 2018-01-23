64160644ea131ad9b358285868129fd1b4ff8137
Josue Perez <che.perez3@gmail.com>
2018-01-23 15:39:38 -0400

    remove Home component form App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './reset.css'
 import './App.css'
 
-import Home from './components/Home';
 import ChannelList from './components/ChannelList';
 import ChannelForm from './components/ChannelForm';
 import Chat from './components/Chat';
