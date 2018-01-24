eb9e608f21b19e7ecf625f06d3b73bb4e45f2079
Josue Perez <che.perez3@gmail.com>
2018-01-24 17:28:53 -0400

    finished formatting date

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -4,11 +4,22 @@ import Moment from 'react-moment'
 class Message extends React.Component {
   render(){
     console.log('msg', this.props)
+let date = null;
+
+if(this.props.date >= this.props.date + (604800*2)){
+  date = (
+    <Moment format="MMM Do YYYY" date={this.props.date} />
+  )
+} else {
+  date = (
+    <Moment fromNow date={this.props.date} />
+  )
+}
+
     return (
       <div className="message">
-        <Moment fromNow date={this.props.date} />
-        <p>{this.props.name}:  <span className="chat-message">{this.props.message}</span></p>
-
+        {date}
+        <p>{this.props.name}: <span className="chat-message">{this.props.message}</span></p>
       </div>
     )
   }
